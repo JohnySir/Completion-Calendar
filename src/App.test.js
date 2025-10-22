fc8fa4e10f882e353d947e068730e89ea715b36b
The Lazy Johny <98986175+JohnySir@users.noreply.github.com>
2025-10-23 01:13:35 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Completion Calendar heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Completion Calendar/i);
+  expect(headingElement).toBeInTheDocument();
 });
